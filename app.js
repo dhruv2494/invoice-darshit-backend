de5eb681cc38303f2a4887b1aaddf5c5c3b64226
Dhruv Bhuva <dhruv.bhuva@crestinfotech.co.in>
2025-04-24 17:33:28 +0530

    changes

--- app.js
+++ app.js
@@ -5,22 +5,22 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const path = require("path");
 const { connectDatabase } = require("./database/db");
-const routes = require('./router/index');
-const errorMiddleware = require('./utils/default/globalErrorHandler');
-dotenv.config()
-const app = express();  
+const routes = require("./router/index");
+const errorMiddleware = require("./utils/default/globalErrorHandler");
+dotenv.config();
+const app = express();
 const port = 7001;
 
 //CORS to handle credentials + specific origin
 const corsOptions = {
-origin: '*',
-credentials: true,
-methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
 app.use(cors(corsOptions));
 
 //Explicitly handle preflight requests
-app.options('*', cors(corsOptions));
+app.options("*", cors(corsOptions));
 // Body parser and cookie parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,18 +29,17 @@ app.use(cookieParser());
 // Database Connection
 const pool = connectDatabase();
 app.use((req, res, next) => {
-    req.pool = pool;
-    next();
-  });
+  req.pool = pool;
+  next();
+});
 
-
-
-
-//Routes and error Middleware 
+//Routes and error Middleware
 app.use("/", routes);
 app.use(errorMiddleware);
-
-//Start the Server	
+app.get("/", (req, res) => {
+  res.send("Invoice Server Is Running")
+});
+//Start the Server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
